bf0da93b2726413c5b17c396066c73cb1c4950ea
Matej Vrzala <vrzala.matej@gmail.com>
2017-08-31 15:28:14 +0200

    search box moved to filter box close #12

--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,56 @@
 (function($) {
-    "use strict";
 
-    $('body').scrollspy({
-        target: '.fixed-top',
-        offset: 60
-    });
+"use strict";
 
-    new WOW().init();
-    
-    
-    $('#collapsingNavbar li .hideOnClick').click(function() {
-        /* always close responsive nav after click */
-        $('.navbar-toggler:visible').click();
-    });
+$('body').scrollspy({
+    target: '.fixed-top',
+    offset: 60
+});
 
-    $('#subscribe').submit(function(e) {
-        e.preventDefault();
-
-        var form = $( this );
-        $.ajax({
-            type: form.attr('method'),
-            url: form.attr('action'),
-            data: form.serialize(),
-            success: function (data) {
-                alert(data)
-                console.log(this.data)
-            },
-            error: function (xhr, desc, err) {
-                alert('error')
-                console.log(err)
-                console.log(xhr)
-                console.log(this.url)
-                console.log(this.type)
-                console.log(this.data)
-            }
-        });
-
-    })
-
-    $(document).on('click', '#searchButton', function(e) {
+new WOW().init();
 
 
+$('#collapsingNavbar li .hideOnClick').click(function() {
+    /* always close responsive nav after click */
+    $('.navbar-toggler:visible').click();
+});
 
-        $("#search").show('slow');
-        
+$('#subscribe').submit(function(e) {
+    e.preventDefault();
 
-        setTimeout(function() {
-            $("#searchButton").prop("type", "submit");
-        }, 100);
-
-
-    });
-
-    $('body').click(function(e) {
-        if (!$(e.target).is('#search') && !$(e.target).is('#searchButton')) {
-            $('#search').hide('slow');
-            $("#searchButton").prop("type", "button");
+    var form = $( this );
+    $.ajax({
+        type: form.attr('method'),
+        url: form.attr('action'),
+        data: form.serialize(),
+        success: function (data) {
+            alert(data)
+            console.log(this.data)
+        },
+        error: function (xhr, desc, err) {
+            alert('error')
+            console.log(err)
+            console.log(xhr)
+            console.log(this.url)
+            console.log(this.type)
+            console.log(this.data)
         }
-
     });
 
+})
 
-        var bLazy = new Blazy({
+var bLazy = new Blazy({
 
-          success: function(element){
-            setTimeout(function(){
-            // We want to remove the loader gif now.
-            // First we find the parent container
-            // then we remove the "loading" class which holds the loader image
-            var parent = element.parentNode;
-            parent.className = parent.className.replace(/\bloading\b/,'');
-            }, 1000);
-            }
-       });
-
-
+  success: function(element){
+    setTimeout(function(){
+    // We want to remove the loader gif now.
+    // First we find the parent container
+    // then we remove the "loading" class which holds the loader image
+    var parent = element.parentNode;
+    parent.className = parent.className.replace(/\bloading\b/,'');
+    }, 1000);
+    }
+});
 
 
 })(jQuery);
