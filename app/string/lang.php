fe54f0d798377408f3cecbf72108f1c59067ef88
Matej Vrzala <vrzala.matej@gmail.com>
2017-10-20 14:20:06 +0200

    fix empty lang values

--- app/string/lang.php
+++ app/string/lang.php
@@ -7,7 +7,8 @@ $sk = Locale::loadLang("sk");
 if (COUNTRY_CODE == 'cz') {
   $cz = Locale::loadLang("cz");
 
-	$lang = array_merge($sk, $cz);
+	$lang = array_merge($sk, array_filter($cz));
 } else {
 	$lang = $sk;
 }
+
